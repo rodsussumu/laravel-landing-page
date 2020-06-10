c8916cb34e259d832324de55b8d54d4848077b82
Rodrigo Tanaka <rstanaka@aluscseduca.com.br>
2020-06-10 18:57:58 -0300

    Adiciona form e conexao com mysql

---
+++ landing-page/app/Http/Controllers/ClienteController.php
@@ -0,0 +1,30 @@
+<?php 
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Cliente;
+
+class ClienteController
+{
+    public function index()
+    {
+        return view('landing-page.index');
+    }
+
+    public function store(Request $request)
+    {
+        $cliente = Cliente::create([
+            'nome' => request('nome'),
+            'email' => request('email'),
+            'telefone' => request('telefone')
+        ]);
+        $request
+            ->session()
+            ->flash(
+                'mensagem',
+                "Enviado com sucesso!!! Entraremos em contato dentro de 3 dias uteis."
+            );
+        return redirect('/home');
+    }
+}
---
+++ landing-page/resources/views/landing-page/index.blade.php
@@ -0,0 +1,14 @@
+@extends("layout")
+
+@section("conteudo")
+    <p>Entre em contato conosco</p>
+    <form action="home" method="post">
+        @csrf
+        <input type="text" id="nome" name="nome" placeholder="Informe seu nome completo">
+        <input type="email" id="email" name="email"placeholder="Informe seu email">
+        <input type="text" id="telefone" name="telefone" placeholder="Informe seu telefone">
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
+@endsection
